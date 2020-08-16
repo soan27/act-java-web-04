b880835ace79ec825a3a227a81676964b1e4d60b
hoangthixoan <hoangthixoan272000@gmail.com>
2020-08-16 17:18:28 +0700

    hoang thi xoan nop bai 13

---
+++ solutions/02_08_2020/hoangthixoan/bai13/bai13_main/Elipse.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai13_main;
+
+/**
+ *
+ * @author Student
+ */
+public class Elipse extends Point{
+    
+    
+}
